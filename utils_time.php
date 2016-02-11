4a98d09170358bbeb224179f24987ac9355322a6
Wayne W <Tree@Winter-Mac.local>
2016-02-12 05:03:31 +0800

    fix locale_day_of_week

--- utils_time.php
+++ utils_time.php
@@ -22,7 +22,7 @@ function locale_day_of_week($ts=null)
     if ($indx > 0 && $indx < 8)
     {
         $days_of_week = array("星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天");
-        return $days_of_week[$indx];
+        return $days_of_week[$indx - 1];
     }
     
     return "";
