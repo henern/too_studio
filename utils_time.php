5f14c81bda00c2f70df52ae696813295323eb058
Wayne W <Tree@Winter-Mac.local>
2016-02-12 04:18:57 +0800

    test

--- utils_time.php
+++ utils_time.php
@@ -3,6 +3,14 @@
 #$g_sec_per_day = 24 * 60 * 60;
 #$g_days_of_week = array("星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天");
 
+function minutes_to_clock_str($mins)
+{
+    $h = floor($mins / 60);
+    $m = $mins % 60;
+    
+    return str_pad($h, 2, "0", STR_PAD_LEFT) . ":" . str_pad($m, 2, "0", STR_PAD_LEFT);
+}
+
 function day_of_week($ts=time())
 {
     $indx = date("N", $ts);
@@ -18,14 +26,6 @@ function day_of_week($ts=time())
 function full_date($ts=time())
 {
     return date("Y-m-d ", $ts) . day_of_week($ts);
-}
-
-function minutes_to_clock_str($mins)
-{
-    $h = floor($mins / 60);
-    $m = $mins % 60;
-    
-    return str_pad($h, 2, "0", STR_PAD_LEFT) . ":" . str_pad($m, 2, "0", STR_PAD_LEFT);
 }
 
 ?>
