3c5d60215b71ac26e907f8daebb0c0d17fc7c7ef
Wayne W <Tree@Winter-Mac.local>
2016-02-12 02:11:30 +0800

    fix path of css

--- book.php
+++ book.php
@@ -9,8 +9,8 @@
 		<meta name="format-detection" content="telephone=no" />
 		<meta name="format-detection" content="email=no" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-        <link rel="stylesheet" href="/css/base.css" type="text/css"/>
-        <link rel="stylesheet" href="/css/book-default.css" type="text/css"/>
+        <link rel="stylesheet" href="./css/base.css" type="text/css"/>
+        <link rel="stylesheet" href="./css/book-default.css" type="text/css"/>
 
 <script>
             var _hip = [['_setPageId', 210001]];
