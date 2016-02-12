402b15be8d7a7c414c5d5424f8390cd021bafbda
Wayne W <Tree@Winter-Mac.local>
2016-02-12 11:25:29 +0800

    page for pay-by-qrcode

--- srvc_pay_qrcode.php
+++ srvc_pay_qrcode.php
@@ -1,12 +1,29 @@
 <?php
-    
-    
+
+
 ?>
 <html>
 <head>
-    <title>微信二维码支付</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="format-detection" content="email=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <link rel="stylesheet" href="./css/base.css" type="text/css"/>
+    <link rel="stylesheet" href="./css/book-default.css" type="text/css"/>
+    
+    <title>微信二维码付款</title>
 </head>
 <body>
-    <img src='./img/too-qrcode-wx-pay.jpeg'/>
+    <header>
+        <div class="title-fullwidth">长按二维码，点击识别二维码</div>
+    </header>
+
+    <section class="content">
+        <div align=center>
+            <img src='./img/too-qrcode-wx-pay.jpeg'/>
+        </div>
+    </section>
 </bdoy>
 </html>
