7ee3f1986b3e6c6bee0a2f1bf7d0b72ddfd98e2f
Wayne W <wangwei@Wang-Weis-MBP.local>
2016-02-24 14:35:48 +0800

    srvc_redis_dump_info

--- redis/srvc_redis_api.php
+++ redis/srvc_redis_api.php
@@ -1,9 +1,15 @@
 <?php
     
-// test code
-$redis = new Redis();
-$redis->connect('127.0.0.1');
-$redis_info = $redis->info();
-var_dump($redis_info);
+function srvc_redis_dump_info()
+{
+    $redis = new Redis();
+    $redis->connect('127.0.0.1');
+    $redis_info = $redis->info();
+
+    $json = json_encode($redis_info);
+    echo "$json";
+}
     
+srvc_redis_dump_info();
+
 ?>
