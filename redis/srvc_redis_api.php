f5e4d11589883301b7ad4b3bbb1d3a335536b408
Wayne W <wangwei@Wang-Weis-MBP.local>
2016-02-24 14:37:28 +0800

    close the reds once finish

--- redis/srvc_redis_api.php
+++ redis/srvc_redis_api.php
@@ -5,7 +5,8 @@ function srvc_redis_dump_info()
     $redis = new Redis();
     $redis->connect('127.0.0.1');
     $redis_info = $redis->info();
-
+    $redis->close();
+    
     $json = json_encode($redis_info);
     echo "$json";
 }
