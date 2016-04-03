8c5b7b7d9349a4d06b5f870ad70a39248ebc1e42
Wayne W <Tree@Winter-Mac.local>
2016-04-03 21:42:29 +0800

    refactor redirect_to_path_with_wx_auth

--- srvc_pay_auth.php
+++ srvc_pay_auth.php
@@ -2,15 +2,6 @@
     
 require_once 'wx_dev.php';
 
-$query_str = $_SERVER["QUERY_STRING"];
-$redirect_uri = TOO_HOME_URL . "/wx/srvc_pay.php?" . $query_str;
-$scope = "snsapi_base";   // 'snsapi_userinfo';
-$state = "TOO-WX";
-
-$auth_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . TOO_WX_APPID .
-            '&redirect_uri=' . urlencode($redirect_uri) . 
-            '&response_type=code&scope=' . $scope . 
-            '&state=' . $state .'#wechat_redirect';
-header("Location:" . $auth_url);
+redirect_to_path_with_wx_auth("wx/srvc_pay.php");
     
 ?>
--- wx_dev.php
+++ wx_dev.php
@@ -157,4 +157,18 @@ function __curl_get_ssl($url, &$error, $second = 10, $headers = array())
     return null;
 }
 
+function redirect_to_path_with_wx_auth($target_uri_path)
+{
+    $query_str = $_SERVER["QUERY_STRING"];
+    $redirect_uri = TOO_HOME_URL . "/" . $target_uri_path . "?" . $query_str;
+    $scope = "snsapi_base";   // 'snsapi_userinfo';
+    $state = "TOO-WX";
+
+    $auth_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . TOO_WX_APPID .
+                '&redirect_uri=' . urlencode($redirect_uri) . 
+                '&response_type=code&scope=' . $scope . 
+                '&state=' . $state .'#wechat_redirect';
+    header("Location:" . $auth_url);
+}
+
 ?>
