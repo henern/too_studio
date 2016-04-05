b7e4a3c1d075ca8f8673aa36c5b459e415103ef3
Wayne W <wangwei34@WangWei-MBP.local>
2016-04-05 18:20:19 +0800

    show map in iframe

--- find_us.php
+++ find_us.php
@@ -11,5 +11,6 @@ log_visitor_info();
 <body>
 <p>任何问题请致电<a href="tel:<?php echo TOO_WX_TEL_KEFU_A ?>">林小姐</a>。</p>
 我们的地址：<a href="<?php echo TOO_WX_MAP_URL ?>"><?php echo TOO_WX_ADDRESS ?></a>。
+<iframe width="100%" height="100%" src="<?php echo TOO_WX_MAP_URL ?>"></iframe>
 </body>
 </html>
