47abc5ca73044fe1e0bffd97734183badfce252f
Wayne W <Tree@Winter-Mac.local>
2016-06-20 02:46:22 +0800

    fix undefined

--- book.php
+++ book.php
@@ -88,7 +88,7 @@ for ($cur_hour = $open_hour_begin;
      $cur_hour <= $open_hour_end; 
      $cur_hour += $open_hour_slot)
 {
-    $timeslot_map[$cur_hour+0] = minutes_to_clock_str($cur_hour);
+    $timeslot_map[$cur_hour] = minutes_to_clock_str($cur_hour);
 }
 ?>
 <html>
@@ -143,7 +143,7 @@ for ($cur_hour = $open_hour_begin;
                 time_select.options.length = 0;
                 for (var timeslot in json_days[date_val])
                 {
-                    var timeslot_val = json_days[date_val][timeslot];
+                    var timeslot_val = ""+json_days[date_val][timeslot];
                     var opt = document.createElement('option');
                     opt.text = str_tsmap[timeslot_val];
                     opt.value = timeslot_val;
